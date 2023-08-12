327ff210fedb55eeecda284b0cb75f405597733f
kacky <t_kaki@nextappli.com>
2023-08-12 17:10:28 +0900

    fix: メンツ手のみに絞ってテストを通す

--- mahjong_core/tests/shanten_test.rs
+++ mahjong_core/tests/shanten_test.rs
@@ -29,7 +29,7 @@ fn parse_testcase(path: &Path) -> io::Result<Vec<(Vec<PaiT>, i32)>> {
             pai_array.push(p);
         }
 
-        let min_shanten = numbers[14].min(numbers[15]).min(numbers[16]);
+        let min_shanten = numbers[14]; // .min(numbers[15]).min(numbers[16]);
 
         pai_vec.push((pai_array, min_shanten));
     }
@@ -39,7 +39,58 @@ fn parse_testcase(path: &Path) -> io::Result<Vec<(Vec<PaiT>, i32)>> {
 
 #[test]
 fn calc_hon_shanten_test() -> Result<(), Error> {
+    let filepath = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/p_hon_10000.txt");
+    let case1 = parse_testcase(&filepath)?;
+    let mut lines = 1;
+
+    for case in case1 {
+        let mut state = PaiState::from(&case.0);
+        println!("case {}", lines);
+
+        assert_eq!(state.get_shanten(), case.1);
+        lines += 1;
+    }
+
+    Ok(())
+}
+
+#[test]
+fn calc_koku_shanten_test() -> Result<(), Error> {
+    let filepath = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/p_koku_10000.txt");
+    let case1 = parse_testcase(&filepath)?;
+    let mut lines = 1;
+
+    for case in case1 {
+        let mut state = PaiState::from(&case.0);
+        println!("case {}", lines);
+
+        assert_eq!(state.get_shanten(), case.1);
+        lines += 1;
+    }
+
+    Ok(())
+}
+
+#[test]
+fn calc_normal_shanten_test() -> Result<(), Error> {
     let filepath = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/p_normal_10000.txt");
+    let case1 = parse_testcase(&filepath)?;
+    let mut lines = 1;
+
+    for case in case1 {
+        let mut state = PaiState::from(&case.0);
+        println!("case {}", lines);
+
+        assert_eq!(state.get_shanten(), case.1);
+        lines += 1;
+    }
+
+    Ok(())
+}
+
+#[test]
+fn calc_tin_shanten_test() -> Result<(), Error> {
+    let filepath = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/p_tin_10000.txt");
     let case1 = parse_testcase(&filepath)?;
     let mut lines = 1;
 
