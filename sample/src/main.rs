5d6f61f0282117bae41f732a8cc64aef61892543
Takaya Kakizaki <t_kaki@nextappli.com>
2024-01-17 19:25:59 +0900

    feat: cliのサンプルプログラムを追加

---
+++ sample/src/main.rs
@@ -0,0 +1,107 @@
+use std::ffi::CString;
+
+use clap::Parser;
+use anyhow::ensure;
+use crossterm::{terminal::{enable_raw_mode, disable_raw_mode}, event::{Event, KeyCode, read}};
+use mahjong_core::mahjong_generated::open_mahjong::{GameStateT, PaiT, ActionType};
+
+#[derive(Parser, Debug)]
+#[command(author, about, version)]
+struct Command {
+    #[arg(short, long)]
+    pai_list_file: Option<String>,
+}
+
+fn hai_to_str(p: &PaiT) -> String {
+    let colors = ["m", "p", "s", "z"];
+
+    let num = p.pai_num;
+
+    let suit = (num / 9) as usize;
+
+    format!("{}{}", colors[suit], num % 9)
+}
+
+fn cmd() -> anyhow::Result<u64> {
+    loop {
+        match read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Char(c) => {
+                        if c == '-' {
+                            return Ok(10);
+                        }
+
+                        if c == '^' {
+                            return Ok(11);
+                        }
+
+                        if c == '\\' {
+                            return Ok(12)
+                        }
+
+                        if c == '0' {
+                            return Ok(9);
+                        }
+
+                        if c >= '1' && c <= '9' {
+                            return Ok(c as u64 - '1' as u64);
+                        }
+
+                        ensure!(false, "{} unknown code", c);
+                    },
+                    KeyCode::Backspace => {
+                        return Ok(13);    
+                    },
+
+                    _ => {
+                        println!("unknown\r");
+                    }
+                }
+            },
+            _ => {}
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Command::parse();
+    let mut game_state: GameStateT = Default::default();
+    let title: CString = CString::new("title")?;
+    let keys =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', '¥'];
+
+    enable_raw_mode()?;
+
+    println!("initialize\r");
+
+    game_state.create(title.as_bytes(), 1);
+    game_state.start();
+
+    loop {
+        let _ = game_state.tsumo();
+
+        let player = game_state.get_player(0);
+
+        player.tehai.iter().enumerate().for_each(|(idx, p)| {
+            print!("{}[{}] ", hai_to_str(p), keys[idx]);
+        });
+        if player.is_tsumo {
+            print!("{}[BS]", player.tsumohai);
+        }
+        println!("\r");
+
+        let command = cmd();
+
+        if let Ok(sutehai) = command {
+            let _ = game_state.action(ActionType::ACTION_SUTEHAI, 0, sutehai);
+            println!("\r");
+        } else {
+            println!("end\r");
+            break;
+        }
+    }
+
+    disable_raw_mode()?;
+
+    Ok(())
+}
