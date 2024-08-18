dd0c6bea8f708fc46d372fb101ef5e05c799e128
kacky <t_kaki@nextappli.com>
2024-08-18 17:58:29 +0900

    fix: game_libのビルドエラーを修正

--- game_lib/src/lib.rs
+++ game_lib/src/lib.rs
@@ -1,7 +1,10 @@
-use std::{mem, os::raw::c_char, ffi::CStr};
+use std::{ffi::CStr, mem, os::raw::c_char};
 
-use mahjong_core::{mahjong_generated::open_mahjong::{GameStateT, Player, ActionType, PaiT}, shanten::PaiState, play_log::PlayLog};
-
+use mahjong_core::{
+    mahjong_generated::open_mahjong::{ActionType, GameStateT, PaiT, Player},
+    play_log::PlayLog,
+    shanten::PaiState,
+};
 
 #[no_mangle]
 pub extern "C" fn get_work_mem_size() -> usize {
@@ -21,7 +24,7 @@ pub unsafe extern "C" fn initialize(ptr: *mut GameStateT, title: *const c_char, 
     let mut play_log = PlayLog::new();
 
     gamestate.clone_from(&GameStateT::default());
-    gamestate.create(title_slice, player_len);
+    gamestate.create(title_slice, player_len, &mut play_log);
     gamestate.start(&mut play_log);
 }
 
@@ -30,8 +33,10 @@ pub unsafe extern "C" fn get_player_shanten(ptr: *mut GameStateT, player_index: 
     let gamestate = ptr.as_mut().unwrap();
 
     let player = gamestate.get_player(player_index);
-    let mut tehai: Vec<PaiT> = player.tehai[0..(player.tehai_len as usize)].iter().cloned().collect();
-
+    let mut tehai: Vec<PaiT> = player.tehai[0..(player.tehai_len as usize)]
+        .iter()
+        .cloned()
+        .collect();
 
     if player.is_tsumo {
         tehai.push(player.tsumohai)
@@ -41,7 +46,12 @@ pub unsafe extern "C" fn get_player_shanten(ptr: *mut GameStateT, player_index: 
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn do_action(ptr: *mut GameStateT, action_type: u32, player_index: usize, param: u32) {
+pub unsafe extern "C" fn do_action(
+    ptr: *mut GameStateT,
+    action_type: u32,
+    player_index: usize,
+    param: u32,
+) {
     let gamestate = ptr.as_mut().unwrap();
     let mut play_log = PlayLog::new();
 
@@ -49,7 +59,11 @@ pub unsafe extern "C" fn do_action(ptr: *mut GameStateT, action_type: u32, playe
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn get_player_state(ptr: *mut GameStateT, player_index: usize, ptr_player: *mut Player) {
+pub unsafe extern "C" fn get_player_state(
+    ptr: *mut GameStateT,
+    player_index: usize,
+    ptr_player: *mut Player,
+) {
     let gamestate = ptr.as_mut().unwrap();
 
     let player = gamestate.get_player(player_index);
